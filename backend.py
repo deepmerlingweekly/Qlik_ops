d457905c9d5fb51b3481d2afae94e5b8c4402d9a
Lorenzo Merli <lorenzo.merli@mediaset.it>
2025-07-29 09:45:23 +0200

    auto assegnamento gruppi TenantAdmin a nuovi space creati

--- backend.py
+++ backend.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 import logging
 from logging import getLogger
+import time
 
 load_dotenv()
 
@@ -15,6 +16,7 @@ def create_qlik_space(environment,space):
     if environment=='test':
         tenant=os.getenv('test_tenant')
         api_key=os.getenv('test_api_key')
+        
     endpoint="{}/api/v1/spaces".format(tenant)
     headers={
         "Authorization": "Bearer {}".format(api_key),
@@ -24,12 +26,44 @@ def create_qlik_space(environment,space):
     payload=space
     try:
         r = requests.post(endpoint,headers=headers,json=payload)
-        print(json.loads(r.text))
+        id=json.loads(r.text)['id']
+        tenant_default_assignment(environment,id)
         return json.loads(r.text)
     except Exception as e:
+        print('error')
         print(e)
 
 def tenant_default_assignment(environment,spaceid):
+    if environment=='prod':
+        tenant=os.getenv('prod_tenant')
+        api_key=os.getenv('prod_api_key')
+        assigneeId_tenant_admin=os.getenv('prod_assigneeId_tenant_admin')
+    if environment=='test':
+        tenant=os.getenv('test_tenant')
+        api_key=os.getenv('test_api_key')
+        assigneeId_tenant_admin=os.getenv('test_assigneeId_tenant_admin')
+    headers={
+        "Authorization": "Bearer {}".format(api_key),
+        "Content-Type": "application/json"
+    }
+    endpoint="{}/api/v1/spaces".format(tenant)
+    assignment_endpoint="{}/{}/assignments".format(endpoint,spaceid)
+    print(assigneeId_tenant_admin)
+    print(tenant)
+    print(os.getenv('test_assigneeId_tenant_admin'))
+    print(assignment_endpoint)
+    assignment_payload={"type":"group","roles":["consumer","facilitator"],"assigneeId":assigneeId_tenant_admin}
+    print(assignment_payload)
+    try:
+        r = requests.post(assignment_endpoint,headers=headers,json=assignment_payload)
+        print(json.loads(r.text))
+        #return json.loads(r.text)
+    except Exception as e:
+        print(e)
+
+
+def list_spaces(environment):
+    print("----------------------------------------")
     if environment=='prod':
         tenant=os.getenv('prod_tenant')
         api_key=os.getenv('prod_api_key')
@@ -41,12 +75,22 @@ def tenant_default_assignment(environment,spaceid):
         "Content-Type": "application/json"
     }
     endpoint="{}/api/v1/spaces".format(tenant)
-    assignment_endpoint="{}/{}/assignments".format(endpoint,spaceid)
-    assignment_payload={"type":"group","roles":["consumer","facilitator"],"assigneeId":os.getenv("assigneeId_tenant_admin_prod")}
+    all_spaces=[]
     try:
-        r = requests.post(assignment_endpoint,headers=headers,json=assignment_payload)
-        print(json.loads(r.text))
-        return json.loads(r.text)
+        while endpoint:
+            time.sleep(0.3)
+            r = requests.get(endpoint,headers=headers)
+            all_spaces.extend(json.loads(r.text)['data'])
+            print(json.loads(r.text))
+            if 'next' in json.loads(r.text)['links'].keys():
+                endpoint=json.loads(r.text)['links']['next']['href']
+            else:
+                endpoint=None
+        sorted_data = sorted(all_spaces, key=lambda x: x["name"])
+        return sorted_data
     except Exception as e:
         print(e)
+
+
+
 
--- pages/02_Group_Manager.py
+++ pages/02_Group_Manager.py
@@ -1,5 +1,24 @@
 import streamlit as st
-
+import backend
 st.title("Qlik-Ops Group Manager")
 
-st.session_state
+#st.session_state
+
+if 'environment_group' not in st.session_state:
+    st.session_state.environment_group = 'test'
+
+col1, col2 = st.columns(2)
+
+with col2:
+     container = st.container(border=True,height=650)
+
+environment_group = col1.toggle("PROD")
+if environment_group:
+    st.session_state.environment_group = 'prod'
+else:
+    st.session_state.environment_group = 'test'
+
+if col1.button("Visualizza Spazi",type="primary"):
+    current_space=backend.list_spaces(st.session_state.environment_group)
+    for space in current_space:
+        container.checkbox(space['name'])
